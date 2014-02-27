d0beb87c2f47189a2976f51a4c25f5bb68e0ca50
Andy Croll <andy@goodscary.com>
2014-02-27 22:29:12 +1300

    Add NameParser to standardize naming
    This is pretty reusable code - gemify it?

---
+++ lib/cineworld_uk/internal/name_parser.rb
@@ -0,0 +1,119 @@
+require 'titleize'
+
+module CineworldUk
+
+  # Internal utility classes: Do not use
+  # @api private
+  module Internal
+
+    # Parses a string to derive a standardized movie title
+    class NameParser
+
+      attr_reader :original_name
+
+      def initialize(name)
+        @original_name = name
+        @name = name
+      end
+
+      # Process the name and return the final string
+      # @return [String]
+      def standardize
+        strip_and_squeeze.
+          ampersands_into_text.
+          into_ampersand_if_second_to_last.
+          remove_indian_languages.
+          remove_screening_details.
+          replace_non_film_prefix.
+          remove_newlines.
+          remove_dates.
+          title_case
+        to_s
+      end
+
+      # The processed name
+      # @return [String]
+      def to_s
+        @name
+      end
+
+      protected
+
+      def ampersands_into_text
+        _replace(/\s(\&amp;|\&)\s/, ' and ')
+        self
+      end
+
+      def into_ampersand_if_second_to_last
+        _replace(/\s(and)\s(\w+)\z/, ' & \2')
+        self
+      end
+
+      def remove_indian_languages
+        languages = ['Malayalam', 'Tamil']
+
+        _remove(/\((#{languages*'|'})\)/i)
+        self
+      end
+
+      def remove_screening_details
+        _remove 'Take 2 Thursday - '
+        _remove 'Autism Friendly Screening: '
+        self
+      end
+
+      def remove_dates
+        _remove(/\-? \d{1,2}\/\d{1,2}\/\d{2,4}/)
+        self
+      end
+
+      def remove_newlines
+        _remove(/\n/)
+        self
+      end
+
+      def replace_non_film_prefix
+        _replace 'Bolshoi Ballet Live -', 'Bolshoi:'
+
+        @name = 'National Theatre: ' + @name.gsub(/\- NT .+ encore/, '') if @name.match /\- NT .+ encore/
+        _replace 'NT Live:', 'National Theatre:'
+
+        _replace 'MET Opera -', 'Met Opera:'
+        _replace 'Royal Ballet Live:', 'Royal Ballet:'
+
+        # fill out Royal Opera House
+        if pure_name_match = @name.match(/Royal Opera Live\: (.+) \-.+/)
+          @name = 'Royal Opera House: ' + pure_name_match[1]
+        end
+        _replace 'Royal Opera Live:', 'Royal Opera House:'
+
+        _replace 'RSC Live:', 'Royal Shakespeare Company:'
+        _remove '(Encore Performance)' # remove rsc-style encore
+
+        _remove ' Theatre Series' # West End
+
+        self
+      end
+
+      def strip_and_squeeze
+        @name = @name.strip.squeeze(' ')
+        self
+      end
+
+      def title_case
+        @name = CineworldUk::Internal::Titleize.titleize(@name)
+        self
+      end
+
+      private
+
+      def _remove(match)
+        @name = @name.gsub(match, '')
+      end
+
+      def _replace(match, replacement)
+        @name = @name.gsub(match, replacement)
+      end
+    end
+  end
+end
