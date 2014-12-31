df57e63ba414b395015ba1f4d8ca6de2b22976b1
Andy Croll <andy@goodscary.com>
2014-12-31 18:26:40 +0000

    v2.1.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 All notable changes to this project will be documented in this file.
 
-## HEAD
+## 2.1.3 - 2014-12-31
 
 ### Fixed
 - better title sanitization of:
--- lib/cineworld_uk/version.rb
+++ lib/cineworld_uk/version.rb
@@ -1,6 +1,6 @@
 # Ruby interface for http://www.cineworld.co.uk
-# @version 2.1.2
+# @version 2.1.3
 module CineworldUk
   # Gem version
-  VERSION = '2.1.2'
+  VERSION = '2.1.3'
 end
